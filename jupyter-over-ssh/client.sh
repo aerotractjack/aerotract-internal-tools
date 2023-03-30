3a62df29e43fb1f07590f7d33d973c82e68fc35f
jack wolf <jackw@aerotractone.com>
2023-03-30 13:02:23 -0700

    set port

--- jupyter-over-ssh/client.sh
+++ jupyter-over-ssh/client.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-cmd="ssh -L localhost:5055:localhost:5055 -p $1 aerotract@$AEROTRACT_PUBLIC_IP" 
+labport="5055"
+if [ ! -z "$2" ]; then
+    labport=$2
+fi
+    
+cmd="ssh -L localhost:$labport:localhost:$labport -p $1 aerotract@$AEROTRACT_PUBLIC_IP" 
 echo $cmd
 $cmd
--- jupyter-over-ssh/server.sh
+++ jupyter-over-ssh/server.sh
@@ -2,8 +2,14 @@
 if [[ "$1" == "-s" || "$1" == "--setup" ]]; then
 	python3 -m pip install jupyterlab
 	sudo apt install tmux
+    exit 0
 fi
 
-cmd="jupyter lab --no-browser --port=5055"
+labport="5055"
+if [ ! -z "$2" ]; then
+    labport=$2
+fi
+
+cmd="jupyter lab --no-browser --port=$labport"
 tmux new -d -s jlab
 tmux send-keys -t jlab.0 "$cmd" ENTER

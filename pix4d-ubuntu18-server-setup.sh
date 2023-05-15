95f81bf082ac03366a65786ccc0e39b8422ae8af
Jack Wolf <jackw@aerotractone.com>
2023-05-15 12:37:27 -0700

    work

--- pix4d-ubuntu18-server-setup.sh
+++ pix4d-ubuntu18-server-setup.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+# ensure user specifies SSH port
 if [[ "$1" == "" ]]; then
 	echo "You forgot to specify the SSH port"
 	echo -e "USAGE:\n\t ./pix4d-ubuntu-server-setup.sh SSHPORT"
 	exit 1
 fi
 
+# install system dependencies/requirements
 sudo apt-get update
 sudo apt-get install openssh-server \
     python3-pip \
@@ -15,8 +17,10 @@ sudo apt-get install openssh-server \
     tmux \
     htop \
 
+# install python libraries
 python3 -m pip install flask
 
+# install gh cli
 type -p curl >/dev/null || (sudo apt update && sudo apt install curl -y)
 curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
     && sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
@@ -24,38 +28,45 @@ curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo 
     && sudo apt update \
     && sudo apt install gh -y
 
+# configure github/gh
 git config --global user.name "jack"
 git config --global user.email "jackw@aerotractone.com"
-
 gh auth login
 
-pushd /home/aerotract
+# clone repos for running/managing orthomosaic export software
+pushd /home/$USER
 mkdir software
 pushd software
-gh repo clone aerotractjack/aerotract-internal-tools internal-tools
-pushd internal-tools/mount-NAS-linux
-sudo ./nas2fstab.sh $USER
-popd
-gh repo clone aerotractjack/orthoq
-gh repo clone aerotractjack/orthoq-dash
-gh repo clone aerotractjack/orthoq-runner
-gh repo clone aerotractjack/orthoq-load-balancer
+for f in aerotract-internal-tools ortho_maker; do
+    gh repo clone aerotractjack/$REPO
 popd
 popd
 
-pushd /home/$USER/NAS/main/Pix4D-SDK-install
+# mount the NAS
+pushd /home/$USER/software
+mv aerotract-internal-tools internal-tools
+pushd internal-tools
+sudo ./nas2fstab $USER
+popd
+popd
+
+# install the pix4d sdk and vscode for remote editing
+pushd /home/$USER/NAS/main/pix4d_server/sdk_install
 sudo apt-get install ./python3-pix4dengine_1.4.3_amd64.deb
 sudo apt install ./code_1.77.0-1680085573_amd64.deb
 sudo cp certs/* /etc/ssl/certs
 popd
-
 code ext install ms-vscode-remote.vscode-remote-extensionpack
 
-sudo chmod a+w /etc/ssh/sshd_config
+# edit the sshd_config file to host on our ssh port
+sudo chmod root:root /etc/ssh/sshd_config
+sudo chmod 644 /etc
 sudo echo "Port $1" >> /etc/ssh/sshd_config
 sudo systemctl restart ssh
 sudo systemctl status ssh
 
+# set some system aliases and variables
 echo "alias cl=\"clear\"" >> ~/.bashrc
 echo "alias p3=\"python3\"" >> ~/.bashrc
+echo "export ORTHOQ_FINISHED_DIR=\"/home/$USER/NAS/main/ORTHOQ_FINISHED\"" >> ~/.bashrc
 source ~/.bashrc

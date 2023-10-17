a6560419ee01dc3a1e67bc8738d965f8ad6caef5
jack <jackw@aerotractone.com>
2023-10-17 13:30:27 -0700

    work

---
+++ restart-service.sh
@@ -0,0 +1,4 @@
+#/bin/bash
+
+/home/aerotract/software/internal-tools/service-destroy.sh $1
+/home/aerotract/software/internal-tools/service-setup.sh $1
--- startup_scripts/sandra.sh
+++ startup_scripts/sandra.sh
@@ -3,28 +3,27 @@
 # this script assumes that the dashboard, database, storage manager, zipper app 
 #   are all cloned into the /home/aerotract/software directory
 #   and have a run.sh file of their own
+exec &>> /tmp/sandra.log
 
 base="/home/aerotract/software"
+tmux="/usr/bin/tmux"
 pushd $base
+
+$tmux start-server
+
+sleep 10
 
 for d in AerotractDashboard AerotractDatabase StorageManager zipper-app; do 
     echo "Processing: $base/$d"
 
-    # Check if a tmux session with the name $d exists
-    tmux has-session -t $d 2>/dev/null
+    $tmux has-session -t $d 2>/dev/null
 
-    # $? is a special variable that captures the exit status of the last command.
-    # If the exit status is 0, it means the tmux session exists, so we kill it.
     if [ $? != 1 ]; then
-        tmux kill-session -t $d
+        $tmux kill-session -t $d
     fi
 
-    # Start a new tmux session detached (-d) with the session name (-s) as the directory's name.
-    # The command to execute is to navigate to the directory and execute run.sh.
-    tmux new-session -d -s $d "cd $base/$d && ./run.sh"
-    
-    # If you wanted to print the contents of run.sh like in your original script, you can uncomment the line below:
-    # cat $base/$d/run.sh
+    $tmux new-session -d -s $d -f /dev/null "/bin/bash -c 'cd $base/$d && ./run.sh'" &>> /tmp/sandra_tmux_cmd.log
+
 done
 
 popd
